599a4d81a14ab742b80801437070329bbf0b2c80
krishna7462 <117273752+krishna7462@users.noreply.github.com>
2023-02-12 15:55:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ function fn1(){
 	let randomNum = Math.random()*(max - min)+min;
 	let n =Math.floor(randomNum);
 	document.getElementById('num').innerText = n;
+	let guessnum = document.getElementById('guess').innerText;
+	if(guessnum==n){
+		document.getElementById("respond").innerText="Hot";
+	}
+	else{
+		document.querySelector("#respond").innerText="Cold";
+	}
 }
