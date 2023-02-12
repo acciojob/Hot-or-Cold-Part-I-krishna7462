9d80d97e831ca0ace1eee0d0350de05d52823ed6
krishna7462 <117273752+krishna7462@users.noreply.github.com>
2023-02-12 14:50:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
-
+function fn1(){
+	let max =20;
+	let min = -20;
+	let randomNum = Math.random()*(max - min)+min;
+	let n =Math.floor(randomNum);
+	document.getElementById('num').innerText = n;
+}
